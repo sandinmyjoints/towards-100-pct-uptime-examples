6b56ac12f06a1a6627135662df1b4851065bb73c
William Bert <william@spanishdict.com>
2013-10-06 18:16:27 -0400

    Add first domain example.

---
+++ app-http-sync-domain.js
@@ -0,0 +1,23 @@
+var http = require('http'),
+    path = require('path'),
+    domain = require("domain");
+
+var PORT = process.env.PORT || 1025;
+
+var d = domain.create();
+
+d.on("error", function(err){
+    response.writeHead(500, {'Content-Type': 'text/plain'});
+    response.end(path.basename(__filename) + ': Error (domain): ' + ex.message + '\n');
+});
+
+d.run(function() {
+    http.createServer(function (request, response) {
+        throw new Error("uh-oh");
+        response.writeHead(200, {'Content-Type': 'text/plain'});
+        response.end(path.basename(__filename) + ': Hello World\n');
+
+    }).listen(PORT);
+});
+
+console.log('Server running at http://127.0.0.1:' + PORT + '/');
