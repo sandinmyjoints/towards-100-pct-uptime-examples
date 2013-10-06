287889bfbd275ebb4bbf5d24cd2f32f264e2e998
William Bert <william@spanishdict.com>
2013-10-06 18:15:45 -0400

    Run 'em.

--- index.js
+++ index.js
@@ -1,5 +1,33 @@
-var fs = require('fs');
+#!/usr/bin/env node
 
-// TODO: open index-*.js, run on successive ports, say what is running
-//files = fs.readDirSync(__dirname);
-//files = files.each(function(file) { file[]})
+var fs = require('fs'),
+    spawn = require('child_process').spawn,
+    glob = require("glob");
+
+var PORT_START = 1025,
+    NODE = "/Users/william/.nvm/v0.10.18/bin/node";
+
+var servers = [];
+var n = -1;
+
+var runEm = function(err, files){
+    files.forEach(function(file){
+        console.log("starting " + file);
+
+        var port = PORT_START + ++n;
+
+        servers[n] = spawn(NODE, [file], {env: {PORT: port}});
+        var prefix = file + " (child " + n + ", port " + port + ") ";
+        var logger = function(msg){
+            console.log(prefix + " " + msg);
+        };
+
+        servers[n].stdout.on("data", logger);
+        servers[n].stderr.on("data", function(data) {logger("error: " + data);});
+        servers[n].stderr.on("close", function() {logger("closed");});
+    });
+};
+
+// main
+if(require.main == module)
+    glob("app-*.js", runEm);
